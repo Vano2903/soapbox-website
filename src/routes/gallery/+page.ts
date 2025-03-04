279fcd0ff61903a789fc837c5ad525ca20617fbf
vano2903 <davidevanoncini2003@gmail.com>
2025-03-04 23:29:26 +0100

    feat: add redirect logic and gallery type

--- src/routes/gallery/+page.ts
+++ src/routes/gallery/+page.ts
@@ -1,6 +1,31 @@
 import type { PageLoad } from './$types';
 import { goto } from '$app/navigation';
 
+type ImageType = {
+	width: number;
+	height: number;
+	thumb: string;
+	full: string;
+};
+
+type GroupType = {
+	name: string;
+	location: string;
+	author: string;
+	date: string;
+	number: number;
+	id: string;
+	images: ImageType[];
+};
+
+type ResponseType = {
+	action: 'redirect' | 'none';
+	setQueryParameter: boolean;
+	availableYears: number[];
+	currentReturnedYear: number;
+	groups: GroupType[];
+};
+
 export const load: PageLoad = async ({ url, fetch }) => {
 	console.log(url.searchParams.get('year'));
 
@@ -18,14 +43,22 @@ export const load: PageLoad = async ({ url, fetch }) => {
 	if (!yearResponse.ok) {
 		console.log('year does not exist, redirecting to latest year');
 		goto(`/gallery?year=${latest}`);
-		return;
+		// return;
+		return {
+			action: 'redirect',
+			setQueryParameter: true,
+			availableYears: [],
+			currentReturnedYear: 0,
+			groups: []
+		} as ResponseType;
 	}
 	const yearData = await yearResponse.json();
 	// const eventLeaderboard = (await eventLeaderboardJson.json()) as EventLeaderboardType;
 	return {
+		action: 'none',
 		setQueryParameter: shouldSetQueryParam,
 		availableYears: [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
 		currentReturnedYear: parseInt(year, 10),
-		groups: yearData.groups
-	};
+		groups: yearData.groups ?? []
+	} as ResponseType;
 };

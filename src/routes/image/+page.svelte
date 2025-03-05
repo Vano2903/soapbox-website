4b859a16bf4dfad10f5d55730190742fc8a15488
vano2903 <davidevanoncini2003@gmail.com>
2025-03-05 13:15:18 +0100

    feat: add image page, WIP

---
+++ src/routes/image/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Props {
+		image: string;
+		width: number;
+		height: number;
+		event: string;
+		shotAt: string;
+		next?: string;
+		prev?: string;
+		author: string;
+		collection: string;
+		location: string;
+	}
+</script>
+
+image
+
+<style>
+</style>

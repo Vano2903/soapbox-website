12552e44ac60091a580c509fe9f1eabd222cf435
vano2903 <davidevanoncini2003@gmail.com>
2025-03-06 20:49:46 +0100

    feat: add auth sequence as server hook, checks connection to backend, auth and authorization

---
+++ src/hooks.server.ts
@@ -0,0 +1,21 @@
+import type { Handle, HandleServerError } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+import pocketbase from '$lib/server/hooks/pocketbase';
+import authentication from '$lib/server/hooks/authentication';
+import authorization from '$lib/server/hooks/authorization';
+// import errorsHandler from '$lib/server/hooks/errors';
+
+// import goCatch from '$lib/utils/goCatch'
+
+// globalThis.goCatch = goCatch
+
+export const handle: Handle = sequence(pocketbase, authentication, authorization);
+
+export const handleError = async ({ error, status, event }) => {
+	// console.error('error', error);
+	console.error('error', { error, status, event });
+	// console.error('status', status);
+	// console.error('event', event);
+	// return errorsHandler({ error, status, event });
+};

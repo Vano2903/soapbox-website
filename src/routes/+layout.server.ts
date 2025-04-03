1ab1313fb948db6e5e9aed8e8b91a8269a63d455
vano2903 <davidevanoncini2003@gmail.com>
2025-04-03 17:46:51 +0200

    feat: return pbUri in the layout as it is often used

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,12 @@
 import type { LayoutServerLoad } from './$types';
 import type { User } from '$lib/types/user';
+import { env } from '$env/dynamic/public';
+import { PUBLIC_PB_INSTANCE } from '$env/static/public';
 
 export const load: LayoutServerLoad = async ({ request, locals }) => {
 	const user = locals.user;
-
-	return { user } as {
+	return { user, pbUri: PUBLIC_PB_INSTANCE } as {
 		user: User;
+		pbUri: string;
 	};
 };

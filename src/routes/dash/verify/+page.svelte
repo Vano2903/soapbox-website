c03ed7a4b7529694c65ecc53800878dd3e16ced9
vano2903 <davidevanoncini2003@gmail.com>
2025-04-03 17:46:22 +0200

    feat: wip: verify page, should add polling or realtime to check when the user is done verifiying the email

---
+++ src/routes/dash/verify/+page.svelte
@@ -0,0 +1,3 @@
+vai nella tua posta elettronica e clicca sul link per verificare la mail. Quando hai fatto clicca su
+questo pulsante per continuare.
+<a class="btn btn-info" href="/dash/onboarding">completa la registrazione</a>

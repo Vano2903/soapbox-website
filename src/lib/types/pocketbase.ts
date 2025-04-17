512ea2a7a0456b2b8b0737e6718ce3ec3bcf412b
vano2903 <davidevanoncini2003@gmail.com>
2025-04-17 18:42:29 +0200

    feat: add team type and pocketbase team collection

--- src/lib/types/pocketbase.ts
+++ src/lib/types/pocketbase.ts
@@ -1,10 +1,12 @@
 import PocketBase, { type RecordService } from 'pocketbase';
 import type { User } from '$lib/types/user';
+import type { Team } from './team';
 
 export interface TypedPocketBase extends PocketBase {
 	collection(idOrName: string): RecordService; // default fallback for any other collection
 	collection(idOrName: 'users'): RecordService<User>;
 	collection(idOrName: 'usernames'): RecordService<{ id: string; username: string }>;
+	collection(idOrName: 'teams'): RecordService<Team>;
 	//   collection(idOrName: "activities"): RecordService<Activity>;
 	//   collection(idOrName: "tickets"): RecordService<Ticket>;
 	//   collection(idOrName: "registrations"): RecordService<Registration>;
--- src/lib/types/team.ts
+++ src/lib/types/team.ts
@@ -2,13 +2,32 @@ import type { User } from './user';
 
 export type Team = {
 	id: string;
+	nick: string;
 	name: string;
 	description: string;
 	number: number;
 	created: Date;
 	updated: Date;
+	expanded: boolean; // if expanded it means that members is of type User[]
+	// if not expanded it means that members is of type string[]
 	members: string[] | User[];
 	owner: string | User;
 	logo?: string;
 	banner?: string;
+};
+
+export type TeamInvitation = {
+	team: string | Team;
+	// max number of users that can join the team with this invatation, -1 for unlimited
+	uses: number;
+	expiration: Date;
+	expanded: boolean;
+	joined?: string[] | User[];
+	//can de disabled by the user and it's equivalent to deleting the invitation
+	// it will be marked as disabled when the expiration date is reached or
+	// when the joined users are equal to the uses
+	disabled: boolean;
+
+	created: Date;
+	updated: Date;
 };

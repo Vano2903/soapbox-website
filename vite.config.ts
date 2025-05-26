4a885c5759468216f7548445c9c0e20f5dc66922
vano2903 <davidevanoncini2003@gmail.com>
2025-05-26 17:03:02 +0200

    fix: stop vite from reloading by reading pocketbase wal

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), enhancedImages()]
+	plugins: [sveltekit(), tailwindcss(), enhancedImages()],
+	// server: {
+	// 	host: '0.0.0.0',
+	// 	port: 8887,
+	// 	strictPort: true,
+	// 	hmr: false
+	// }
+	server: {
+		watch: {
+			ignored: ['**/pocketbase/**']
+		}
+	}
 });

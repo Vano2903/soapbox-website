96912b9527319a67214bb646fdbb278f5fa513b8
vano2903 <davidevanoncini2003@gmail.com>
2025-05-26 17:04:21 +0200

    feat: WIP: markdown editor and markdown preview

---
+++ src/lib/components/ui/markdownEditor/markdownEditor.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Carta, MarkdownEditor } from 'carta-md';
+	import 'carta-md/default.css'; /* Default theme */
+	import DOMPurify from 'isomorphic-dompurify';
+
+	const carta = new Carta({
+		sanitizer: DOMPurify.sanitize
+		// theme: 'github-dark',
+	});
+
+	// let theme = $state('github-light');
+
+	let {
+		theme = $bindable(),
+		value = $bindable(),
+		...rest
+	}: {
+		theme: string;
+		value: string;
+	} = $props();
+</script>
+
+<MarkdownEditor {carta} bind:value />

a143fc4fae80566d8b9267fe05d1eaef8140a7d7
vano2903 <davidevanoncini2003@gmail.com>
2025-06-20 16:56:39 +0200

    feat: wip searchbox

--- src/routes/dash/team/+page.svelte
+++ src/routes/dash/team/+page.svelte
@@ -8,6 +8,18 @@
 {#if !team || team.totalItems == 0}
 	<p class="text-center">Non sei ancora in nessun team</p>
 {:else}
+	<h1 class="mb-4 text-3xl font-bold">I tuoi team</h1>
+
+	<!-- search bar -->
+	<div class="flex">
+		<div class="form-control mb-4">
+			<label class="label">
+				<span class="label-text">Cerca tra i tuoi team:</span>
+			</label>
+			<input type="text" disabled placeholder="secondo te funziona ora?" class="input input-bordered" />
+		</div>
+		<a href="/dash/team/new" class="btn max-w-64 bg-red-600">Crea un Nuovo Team</a>
+	</div>
 	{#each team.items as t}
 		<div class="card bg-base-100 mb-4 w-full shadow-xl">
 			<div class="card-body">

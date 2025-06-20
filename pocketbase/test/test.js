ec83957593e8788129762d40e446bdf8bd4d0ceb
vano2903 <davidevanoncini2003@gmail.com>
2025-06-20 12:52:52 +0200

    feat: make the update of token easier

--- pocketbase/test/test.js
+++ pocketbase/test/test.js
@@ -1,20 +1,14 @@
 import PocketBase from 'pocketbase';
 
 // dont worry, this token is not real, its from my local dev instance
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJfcGJfdXNlcnNfYXV0aF8iLCJleHAiOjE3NDc3NjYzNTUsImlkIjoiYjU0MTR1bWI1OTI2NTAzIiwicmVmcmVzaGFibGUiOmZhbHNlLCJ0eXBlIjoiYXV0aCJ9.Ur2r0_FNjENsIE4A3b7YVxwuD_CfiqIMKteDQFKdg0Q`;
+const token = `
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJfcGJfdXNlcnNfYXV0aF8iLCJleHAiOjE3NDc3NjYzNTUsImlkIjoiYjU0MTR1bWI1OTI2NTAzIiwicmVmcmVzaGFibGUiOmZhbHNlLCJ0eXBlIjoiYXV0aCJ9.Ur2r0_FNjENsIE4A3b7YVxwuD_CfiqIMKteDQFKdg0Q
+`.trim();
 const pb = new PocketBase('http://0.0.0.0:8080');
 
 pb.authStore.save(token, null);
 
 try {
-	// pb.collection('teams').create({
-	// 	nick: 'test',
-	// 	name: 'test name'
-	// });
-
-	// pb.collection('teams').update('33h4xlx90dw82n2', {
-	// 	'members+': ['2c4pd1xns3mdk9r']
-	// });
 	const invite = 'jixa99io2qq7e33';
 	const userid = 'b5414umb5926503';
 

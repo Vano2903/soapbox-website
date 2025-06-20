bd22d16c5ebe28415a3958f0986fccf314b9e6a7
vano2903 <davidevanoncini2003@gmail.com>
2025-06-20 16:27:41 +0200

    feat: team page and loading of team

--- src/routes/dash/team/[slug]/+page.svelte
+++ src/routes/dash/team/[slug]/+page.svelte
@@ -1,39 +1,77 @@
 <script lang="ts">
 	const { data } = $props();
-	const {slug,team} = data;
+	const { slug, team } = data;
 </script>
 
-questa è la pagina del team con slug {slug}
+<div class="p-2">
+	questa è la pagina del team con slug {slug}
 
-la pagina del tuo team
+	la pagina del tuo team
 
-<button class="btn">Crea un nuovo team</button>
-<button class="btn">Entra a far parte di un nuovo team</button>
-<button class="btn">lascia il team</button>
-<button class="btn">elimina team</button>
-<button class="btn">trasferisci la proprietà del team</button>
-
-<div class="overflow-x-auto">
-	<table class="table w-full">
-		<thead>
-			<tr>
-				<th>Nome</th>
-				<th>Ruolo</th>
-				<th>Azioni</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each team.members as member}
-				<tr>
-					<td>{member.name} {member.lastName}</td>
-					<td>{member.role}</td>
-					<td>
-						<button class="btn btn-info">Modifica</button>
-						<button class="btn btn-error">Rimuovi</button>
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+	<div class="mx-3 my-3 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+		<button class="btn">Crea un nuovo team</button>
+		<button class="btn">Entra a far parte di un nuovo team</button>
+		<button class="btn">lascia il team</button>
+		<button class="btn">elimina team</button>
+		<button class="btn">trasferisci la proprietà del team</button>
+	</div>
+	<div class="overflow-x-auto">
+		<div class="flex justify-center">
+			<div class="w-1/2">
+				<div>
+					<h1 class="flex justify-center text-3xl">Membri</h1>
+				</div>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>Azioni</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each team.expand.members as member}
+							<tr>
+								<td>{member.name} {member.lastName}</td>
+								<!-- <td>{member.role}</td> -->
+								<td>
+									<button class="btn btn-info">Modifica</button>
+									<button class="btn btn-error">Rimuovi</button>
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<div class="divider"></div>
+	<div class="overflow-x-auto">
+		<div class="flex justify-center">
+			<div class="w-1/2">
+				<div>
+					<h1 class="flex justify-center text-3xl">Inviti</h1>
+				</div>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>Azioni</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each team.expand.members as member}
+							<tr>
+								<td>{member.name} {member.lastName}</td>
+								<!-- <td>{member.role}</td> -->
+								<td>
+									<button class="btn btn-info">Modifica</button>
+									<button class="btn btn-error">Rimuovi</button>
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
-<div class="divider"></div>

2b9d63545b5fadec89163c908fedd130e0ddfbab
MoraGames <andreamorandi2612@gmail.com>
2025-06-30 19:42:05 +0200

    feat: implement GSheetsAPI polling for livedata updates

--- src/routes/championships/+page.ts
+++ src/routes/championships/+page.ts
@@ -1,29 +1,5 @@
-import type { PageLoad } from './$types';
+// import type { PageLoad } from "../$types";
 
-import type { LiveLeaderboardsType } from '$tsTypes/championships/liveLeaderboards';
-import type { ChampionshipLeaderboardType } from '$tsTypes/championships/championshipLeaderboard';
-import type { EventLeaderboardType } from '$tsTypes/championships/eventLeaderboard';
-
-export const load: PageLoad = async ({
-	fetch,
-	params
-}): Promise<{
-	liveLeaderboards: LiveLeaderboardsType;
-	championshipLeaderboard: ChampionshipLeaderboardType;
-	eventLeaderboard: EventLeaderboardType;
-}> => {
-	const liveLeaderboardsJson = await fetch('test-jsonfiles/liveLeaderboards.json');
-	const championshipLeaderboardJson = await fetch('test-jsonfiles/championshipLeaderboard.json');
-	const eventLeaderboardJson = await fetch('test-jsonfiles/eventLeaderboard.json');
-
-	const liveLeaderboards = (await liveLeaderboardsJson.json()) as LiveLeaderboardsType;
-	const championshipLeaderboard =
-		(await championshipLeaderboardJson.json()) as ChampionshipLeaderboardType;
-	const eventLeaderboard = (await eventLeaderboardJson.json()) as EventLeaderboardType;
-
-	return {
-		liveLeaderboards,
-		championshipLeaderboard,
-		eventLeaderboard
-	};
-};
+// export const load: PageLoad = async () => {
+// 	updateSheet();
+// };

9e8dace11897b7d1d5a82ac0789b2898a0b51491
vano2903 <davidevanoncini2003@gmail.com>
2025-07-04 03:48:01 +0200

    feat: WIP: progress bar component

---
+++ src/lib/components/ui/progress/progressBar.svelte
@@ -0,0 +1,118 @@
+<!-- https://svelte.dev/playground/7b05d57dcdc04f49be72844e4b2825b3?version=5.35.2#H4sIAAAAAAAAA70Y7Y7buPFVBkpT2RtLtpPdu0Rn-y69XoAAbRog_XOIg4CWKJsXmlRJateOoHcvSJEyJcveBD00yWal4XzPcGY0VcDQHgdJ8OEeU4VhX1JFpMKFhJyLvQwmQU4olkHysQrUsdCoGhBMHOHrooilIQ4mwQZJPARPOVOYKRkkwUKmghRqtWZrRfYFFwrecLGHXPA9hPFUv1jC8CcP6b3gW4Gl_BsSLa4H65BQrKAxYgkfw7cs5-EEwtdZppH143t03GOm9OOvnOVE7JEinIWfJpCWQmCmXqeK3GNYwl-kQgqP5uMJFJ4K7cHYSNQ_KWdSwQ6xjGKnGSxhpDV5y1KBtcgxLFdQGRLl8Yu7ZD0ajV43YhbTk__YYo8IM55cZOQeUoqkXK6NtxFhWKyDVSNp4XuvMq6pYUNYlnTNNSC1I3JZecrVU8vGMjPxQobis-a1bDh-7PCK5p8auobG109jR5tSKc5aDddq0UBarI1i6wA4SylJvyyrkfFcz03RfFxDRiTaUJwtq17sljCvV-8Fvl9MG97_k6xnj8gyPogpZlu1q1fv8EH1xC6mGblfOT-6NwAdVBdItpDqSLH12y8290tBR-FOqUIm02nOmZLxlvMtxaggMk75fppK-XOO9oQel_8sKflrRmRB0XEpH1ARnlL0xqXehh8iSb4Stk1gw0WGRbThh59corG10ho5bC0yatgnEGoB4QQkYjKSWJC8Ya-szARyig8WhijZsogovJcJpJgpLOzJH6VUJD9GtjT0TneYbHcqgflsdr-zMH6PRU75QwI7kmWYWfAeiS1hCcx87eONarXfoPTLVvCSZVHKKRcJPHlx--pltrEMHDDPnSWNRxxL9x4JlJFSJvBD4cxLSyE1bcGJp3zHXYTtsCDKHhUoy4zTXxYHeDFrGTkj7lqIYSLJV5zA_LY49I1LmvvnbFQCMakrdgIyRRSPZvGrl-MzopynpXQ0vFSUMDzgucSl-RUX4pn-2_MD4ypClPIHnLU8TeLF3p2vfJMjxYsE5gLvLSuFD5oH2bJOStSm9Jm7EUwCjRQkSpS4nlzoS14f6Tam7sH1zvSWFaV6QzDN2p5zAp21nMovilDrHlEIXkjbIzSKDtHfkUKn_mG9oZVLIAwnzassRQ-C94hQ771AUj5wkXkg1DQ4D5ISdfRfecmU8CEFlyrlmS8IpQbtc09-ikT2mXENMfEYD7W9D-VmT5Ruen6f0xic4pjy7Wgd_M5LYfwAmXHECtbBxPmlbXW9RmfwXbXWL5HX5IAzaQQvK1-N2kSyekLyTlyWS2hGgtqWZS_IFG0wXVbhO7THoe2Q94iWeFk5DWPtl7YjDhF_aGJ3id6G9iqL33SwLzEwmTBIri_CsgpdaoR1y_D9CTTI05FYtlWCqcQw4Dk3RF1xXosyLMpm6VX7fyXqeIle5_R14ibLL9I3x1dZvLfX4qK77Pmj7nKD5jV3NfcNruWcfyev245EBu_4RdubS_yo2p2h2Onuj3B7LCXaYn982z1f_XuH2Bc48lLfb0h3nEvCtlDKxXT3_NrwBc39XQerN4QRuYPh0UnrPCW56QXapt7E1HSabnW40sH6_b7t7_NTXzZz0g5leuqYmQN4rv8T2w0azSZg_8X682CmBwPzc37ab_93jtP1rqe75CF6IJnamVnoqYVayIu7mTcZmNbdllxj48OOKNyXPYvvBN63w43fh5uj_jxHmJ4Tog3l6ZeuAp5KPR_O4uceKzsf-GNSo3ET97MYJVogEtFWs8NMjRSHDVeK7yfw5PY2_fGHH-Hu6QSe3M02s3lq9HA-Pou0JTiTmuz0RHmeIFeFW4FGuFXkuvCGwBNur051NfDfPe6cjSXdoWfo-MLoM7250b_gBn7RkjKcQ_WvzR84VbX-CC-kO9WzDRbqCBVixxpMrblwZsrT-ZlUgrBtDR-1oE_N-bQZQ6c3Vj5URmjdnNg5y8jSM5SucnpSHY0njZCJaYKwhFA7LTyfwvRPXrJUlzaD-9o8jhjP8NgNYzzDseWjf3kh8WaS4nwgEZzakmjGDqORK5_mpSVpPBLoQrmqGrxkMTUPfp1bqwXRoYNS4uSkbcvGHK6DTrU3v0yJX0yLfomMfV07k3gC3brgZybFufJS2EitLlWCtm74BeO7q4-tJ469V5Ev1hk3N37HtTnfIHXvzeD5n3xxPprNwafzU1buN1jU0F2sfM89aZdgQ4ut9u7Mx8MfKykRKcXS23WddmBDCzB8MB9N370HO8nJeFrq0_g_JRbHD5jiVHHxmtJRGDdYbpmhSN5laNY97gKbjnOy9tkzt4cydF1PrJyddnczhpbJGSNY9pAdpr3kNZiJaliLKLqixQLmjwieQ1_Y2i3lGABAWWQmHJ3v7rbSuVMtQ2M7M3IufkPpbjRqABMg2cHFBuwfko9IdoBFN4XGPsqJZWxK0z-IVHrSH4XNaBmOT7gnJ12lFnjP7_Egg-bRfII2xpiMaxAfSyPH7idH7DI6NnUjNgVHp6tlZwMNkQ7QFEbd-DfgG13-4BmET0N_7dFtF_747GR2vmK95asVUp_vdh1lF99B65U3KT_BKN3ZEoCkeZgAqc9YNsKgIvoKzeBncC4H_bFfrwPzoR4poihuVr01rCrybF77Y7kWZlqkBfXazrnFLtuH1oZny0FZoBRHG6wecLv4K7gkOrcTEJgirXG3pzQzW9Jds33zND2sdJJscM4F9lYbjYJheGkC7K3KTlqjjeS0bOdz0wLvWqV0yz1tIL39nnmkSOHfR9HdafJ0G9Pb1thzQ79GhGX4kEA0H7TzWzem_3cbLlpguVptZvGtBIwk7ljn8vsbPgQd5NWrV8Pjxsk2p6uXXS6PTpA_cyXultIvigNITknWS6zLjhjeU592s42DkgTlCot-YiOlxOhUAMawDmAdPJYI7d07LbTPfXs9dL5utmpbFaH7kevnZjfmcXdJbmM5TDY8N36aBAoR-kBYFiQ5ohLX_wVbrfaAMx0AAA== -->
+<script lang="ts">
+	/**
+	 * @typedef {Object} Props
+	 * @property {any} [steps]
+	 * @property {number} [currentActive]
+	 */
+
+	/** @type {Props} */
+	let { steps = [], currentActive = $bindable(1) } = $props();
+	let circles = $state(),
+		progress = $state();
+
+	export const handleProgress = (stepIncrement: number) => {
+		circles = document.querySelectorAll('.circle');
+		if (stepIncrement == 1) {
+			currentActive++;
+
+			if (currentActive > circles.length) {
+				currentActive = circles.length;
+			}
+		} else {
+			currentActive--;
+
+			if (currentActive < 1) {
+				currentActive = 1;
+			}
+		}
+
+		update();
+	};
+
+	function update() {
+		circles.forEach((circle, idx) => {
+			if (idx < currentActive) {
+				circle.classList.add('active');
+			} else {
+				circle.classList.remove('active');
+			}
+		});
+
+		const actives = document.querySelectorAll('.active');
+
+		progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+	}
+</script>
+
+<div class="progress-container" bind:this={circles}>
+	<div class="progress" bind:this={progress}></div>
+	{#each steps as step, i}
+		<div class="circle {i == 0 ? 'active' : ''}" data-title={step}>{i + 1}</div>
+	{/each}
+</div>
+
+<style>
+	.progress-container {
+		display: flex;
+		justify-content: space-between;
+		position: relative;
+		margin-bottom: 30px;
+		max-width: 100%;
+		width: 350px;
+	}
+
+	.progress-container::before {
+		content: '';
+		background-color: #e0e0e0;
+		position: absolute;
+		top: 50%;
+		left: 0;
+		transform: translateY(-50%);
+		height: 4px;
+		width: 100%;
+		z-index: -1;
+	}
+
+	.progress {
+		background-color: #3498db;
+		position: absolute;
+		top: 50%;
+		left: 0;
+		transform: translateY(-50%);
+		height: 4px;
+		width: 0%;
+		z-index: -1;
+		transition: 0.4s ease;
+	}
+
+	.circle {
+		background-color: #fff;
+		color: #999;
+		border-radius: 50%;
+		height: 30px;
+		width: 30px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 3px solid #e0e0e0;
+		transition: 0.4s ease;
+		cursor: pointer;
+	}
+
+	.circle::after {
+		content: attr(data-title) ' ';
+		position: absolute;
+		bottom: 35px;
+		color: #999;
+		transition: 0.4s ease;
+	}
+
+	.circle.active::after {
+		color: #3498db;
+	}
+
+	.circle.active {
+		border-color: #3498db;
+	}
+</style>

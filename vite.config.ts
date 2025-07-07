32acf2a909391d01cd1a30e20b70df9c31af2571
vano2903 <davidevanoncini2003@gmail.com>
2025-07-07 15:19:27 +0200

    dev: add image preprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), enhancedImages()],
+	plugins: [
+		sveltekit(),
+		tailwindcss(),
+		ViteImageOptimizer({
+			png: { quality: 80 },
+			jpeg: { quality: 75 },
+			webp: { quality: 80 },
+			avif: { quality: 70 },
+			svg: {
+				plugins: [{ name: 'removeViewBox' }, { name: 'sortAttrs' }]
+			}
+		}),
+		,
+		enhancedImages()
+	],
 	// server: {
 	// 	host: '0.0.0.0',
 	// 	port: 8887,
@@ -12,7 +27,7 @@ export default defineConfig({
 	// 	hmr: false
 	// }
 	server: {
-		hmr: { host: "localhost", protocol: "ws", },
+		hmr: { host: 'localhost', protocol: 'ws' },
 		watch: {
 			ignored: ['**/pocketbase/**']
 		}

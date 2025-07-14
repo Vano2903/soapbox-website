8a34e601109954f985f813b5b0ec5ba2a21e9834
vano2903 <davidevanoncini2003@gmail.com>
2025-07-14 02:26:50 +0200

    feat: WIP: public team page

---
+++ src/routes/team/[slug]/+layout.server.ts
@@ -0,0 +1,76 @@
+import type { Team, TeamNonexpand } from '$tsTypes/team';
+import type { UserPublicInfo } from '$tsTypes/user';
+import { redirect, type ServerLoad } from '@sveltejs/kit';
+
+export const load: ServerLoad = async ({ locals, params, url }) => {
+	const { user, pb } = locals;
+
+	if (!params.slug) {
+		redirect(303, '/teams');
+	}
+
+	const [foundUser, err] = (await goCatch(
+		pb.collection('teams').getFirstListItem(`slug="${params.slug}"`)
+	)) as [UserPublicInfo, undefined] | [undefined, Error];
+
+	if (err || !foundUser) {
+		console.error('User not found:', err);
+		throw redirect(303, '/users?error=user-not-found&slug=' + params.slug);
+	}
+
+	const startPathname = '/user/' + params.slug;
+	const path = url.pathname;
+	if (
+		(path.startsWith(startPathname + '/dash') ||
+			path.startsWith(startPathname + '/settings') ||
+			path.startsWith(startPathname + '/new')) &&
+		user?.id !== foundUser.id
+	) {
+		redirect(303, '/user/' + params.slug);
+	}
+
+	console.log('Found user:', foundUser);
+	foundUser.avatarCropped =
+		pb.files.getURL(foundUser, foundUser.avatarCropped || '') ||
+		`https://avatar.iran.liara.run/public?username=${foundUser.nick}`;
+
+	foundUser.bannerCropped = pb.files.getURL(foundUser, foundUser.bannerCropped || '') || undefined;
+
+	let [teams, errTeam] = (await goCatch(
+		pb.collection('teams').getFullList({
+			filter: `members.id?="${foundUser.person}" ||
+				owner.id="${foundUser.person}"`,
+			sort: '-created'
+		})
+	)) as [TeamNonexpand[], undefined] | [undefined, Error];
+
+	// if (errTeam) {
+	// 	console.error('Error fetching teams:', errTeam);
+	// 	throw redirect(303, '/users?error=teams-fetch-error');
+	// }
+
+	let error = {};
+	if (errTeam || !teams) {
+		console.log('Error fetching teams:', errTeam);
+		error = {
+			kind: 'teams',
+			message: 'Errore durante il recupero dei team.'
+		};
+		teams = [];
+	}
+
+	const teamsWithAvatars = teams.map((team) => {
+		team.logoCropped = pb.files.getURL(team, team.logoCropped || '') || undefined;
+		return team;
+	});
+
+	console.log('teams:', teamsWithAvatars);
+
+	return {
+		user: foundUser,
+		teams: teamsWithAvatars || [],
+		error: error,
+		isCurrentUser: user?.id === foundUser.id,
+		slug: params.slug
+	};
+};
---
+++ src/routes/team/[slug]/+page.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+	import type { Team } from '$tsTypes/team';
+	import type { UserNonExpand } from '$tsTypes/user';
+	import { Crown, ExternalLink } from 'lucide-svelte';
+	interface Props {
+		data: {
+			user: UserNonExpand;
+			slug: string;
+			teams: Team[];
+			error: {
+				kind: 'teams' | 'other';
+				message: string;
+			} | null;
+			isCurrentUser: boolean;
+		};
+	}
+	// }
+
+	// const backgroundImage =
+
+	const { data }: Props = $props();
+	// 	user: UserNonExpand;
+	// 	slug: string;
+	// }>();
+	let { user, slug, teams, isCurrentUser } = data;
+</script>
+
+<div class="flex w-full justify-center">
+	<div class="flex w-full flex-col items-center justify-center md:max-w-[80%] lg:max-w-[55%]">
+		<div class="h-2xl line-clamp-3 w-full">
+			{#if user.bannerCropped}
+				<!-- <img src={user.bannerCropped} alt="banner" class="h-full w-full object-fill" /> -->
+				<img src={user.bannerCropped} alt="banner" class="h-full w-full object-fill" />
+			{:else}
+				<div class="h-48 w-full bg-blue-400">
+					<!-- <p class="text-center text-gray-500">No banner available</p> -->
+				</div>
+			{/if}
+		</div>
+
+		<div class="flex w-full items-center justify-center space-x-5 pb-8">
+			<div class="mt-[-1rem] mb-4 h-20 w-20 rounded-full bg-gray-200 md:h-24 md:w-24">
+				<div class="avatar">
+					<div class="w-20 rounded-full ring-1 ring-black md:w-24">
+						<img src={user.avatarCropped} alt="User Avatar" />
+					</div>
+				</div>
+			</div>
+			<div class="mt-[-1rem]">
+				<h2 class="text-2xl font-bold">{user.name} {user.lastName}</h2>
+				<!-- <div class="flex flex-nowrap items-center space-x-2"> -->
+				<a href={'/user/' + user.nick} target="_blank" class="ml-2 font-bold text-red-600">
+					@{user.nick}
+				</a>
+				<!-- <ExternalLink size={16} /> -->
+				<!-- </div> -->
+			</div>
+		</div>
+
+		<!-- bio section -->
+		{#if user.bio && user.bio.length > 0}
+			<div class="w-full px-4">
+				<p class="">Bio:</p>
+				<p class="ml-2 text-lg text-gray-600">{user.bio}</p>
+			</div>
+		{:else if isCurrentUser}
+			<div class="w-full px-4">
+				<p class="text-lg text-gray-600">Non hai ancora scritto una bio.</p>
+			</div>
+		{/if}
+
+		<div class="flex w-full flex-col items-center justify-between px-4">
+			<p class="mt-4 text-3xl font-bold">TEAMS:</p>
+			{#if teams.length == 0}
+				{#if isCurrentUser}
+					<p class="mt-4 text-lg font-semibold">Non fai parte di nessun team</p>
+					<button class="btn btn-primary mt-2">
+						<a href="/teams/new">Creane Uno</a>
+					</button>
+				{:else}
+					<p class="mt-4 text-lg font-normal text-gray-600">
+						Questo account non fa parte di nessun team
+					</p>
+				{/if}
+			{/if}
+			{#each teams as team}
+				<a href={'/team/' + team.slug} class="group my-4 w-full cursor-pointer">
+					<div class="rounded-lg bg-gray-200 p-4 transition-colors group-hover:bg-gray-300">
+						<div class="flex items-start space-x-3">
+							<img src={team.logoCropped} alt="Team Logo" class="size-16 rounded-full" />
+							<div class="flex-1">
+								<div class="justify-right flex items-center space-x-2">
+									<div class="flex flex-nowrap items-center">
+										<p class="group text-lg font-bold transition duration-300">
+											{team.name}
+											<span
+												class="mx-1 block h-0.5 max-w-0 bg-red-600 transition-all duration-500 group-hover:max-w-full"
+											></span>
+										</p>
+
+										<!-- <h3 class="text-xl font-bold group-hover:underline md:text-2xl"></h3> -->
+										{#if team.owner == user.person}
+											<Crown class="ml-1 text-yellow-600" size={16} />
+										{/if}
+									</div>
+									<span class="text-sm font-semibold text-red-600 group-hover:font-bold"
+										>@{team.slug}</span
+									>
+								</div>
+								<p class="mt-1 line-clamp-1 text-gray-700">{team.description}</p>
+							</div>
+						</div>
+					</div>
+				</a>
+			{/each}
+		</div>
+		<!-- <img src={user.avatarCropped} alt="User Avatar" class="mb-4 h-24 w-24 rounded-full" /> -->
+		<!-- 
+		<h1 class="text-3xl font-bold">User Profile</h1>
+		<p class="mt-2 text-lg">
+			Welcome, {user.nick}!
+		</p>
+		<p class="mt-4">This is your profile page.</p>
+		<p class="mt-2">Your slug is: {slug}</p> -->
+		<!-- Add more user-specific information here -->
+	</div>
+</div>

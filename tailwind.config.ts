d55c55ed77d3493b8d9c300fb6d23fe4d5bd3419
vano2903 <davidevanoncini2003@gmail.com>
2025-07-14 02:23:01 +0200

    feat: remove dark mode as it is not handled atm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	darkMode: ['class'],
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	safelist: ['dark']
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}']
+	// safelist: ['dark']
 };
 
 export default config;

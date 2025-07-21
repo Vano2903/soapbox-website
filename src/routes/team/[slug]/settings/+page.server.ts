da8f08a42306b8b222b6f77f4e559343c91d02ec
vano2903 <davidevanoncini2003@gmail.com>
2025-07-21 17:44:10 +0200

    feat: team settings page

---
+++ src/routes/team/[slug]/settings/+page.server.ts
@@ -0,0 +1,119 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import {
+	message,
+	setError,
+	superValidate,
+	type SuperValidated,
+	type Infer,
+	withFiles
+} from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import type { TypedPocketBase } from '$tsTypes/pocketbase';
+import { teamSchema } from '$lib/schemas/teamSchema';
+
+const slugSchema = teamSchema.pick({ slug: true });
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const { user, team } = locals;
+	if (!user) {
+		redirect(303, '/login');
+	}
+
+	const fileUrls = {
+		logoOriginal: team.logo,
+		logoCropped: team.logoCropped,
+		bannerOriginal: team.banner,
+		bannerCropped: team.bannerCropped
+	};
+
+	const form = await superValidate(
+		{
+			...team,
+			logoCroppedInfo: team.logoCrop,
+			bannerCroppedInfo: team.bannerCrop
+		},
+		zod(teamSchema)
+	);
+
+	return { form, fileUrls, user };
+};
+
+async function isTeamNickValid(
+	form: SuperValidated<Infer<typeof slugSchema>>,
+	pb: TypedPocketBase
+): Promise<boolean> {
+	try {
+		const query = `slug="${form.data.slug}"`;
+		console.log('searching team username:', query);
+		await pb.collection('teams').getFirstListItem(query);
+		setError(form, 'slug', 'Esiste già un team con questo username', {
+			overwrite: true
+		});
+		return false;
+	} catch {
+		return true;
+	}
+}
+
+export const actions = {
+	updateAccount: async ({ request, locals }) => {
+		const form = await superValidate(request, zod(teamSchema));
+		console.log('form', form);
+		if (!form.valid) {
+			return fail(400, withFiles({ form }));
+		}
+		console.log(form.data);
+
+		const { pb, user, team } = locals;
+		if (!user) {
+			redirect(303, '/login');
+		}
+
+		const isTeamNickAvailable = await isTeamNickValid(form, pb);
+		console.log('isTeamNickAvailable', isTeamNickAvailable);
+		if (!form.valid || !isTeamNickAvailable) return fail(400, withFiles({ form }));
+
+		try {
+			locals.team = await pb.collection('teams').update(team.id, {
+				name: form.data.name,
+				slug: form.data.slug,
+				description: form.data.bio || '',
+
+				banner: form.data.bannerOriginal,
+				bannerCropped: form.data.bannerCropped,
+				bannerCrop: form.data.bannerCroppedInfo,
+
+				logo: form.data.logoOriginal,
+				logoCropped: form.data.logoCropped,
+				logoCrop: form.data.logoCroppedInfo
+			});
+			return message(form, {
+				type: 'success',
+				text: `Team aggiornato con successo torna alla<a href="/team/dash" class="link">dashboard</a>`
+			});
+		} catch (e) {
+			console.log(e);
+			return message(
+				form,
+				{ type: 'error', text: "Errore durante l'aggiornamento del team, riprova più tardi" },
+				{
+					status: 500
+				}
+			);
+		}
+	},
+
+	checkUsername: async ({ request, locals }) => {
+		const form = await superValidate(request, zod(slugSchema));
+		const { pb, user } = locals;
+		if (!user) {
+			redirect(303, '/login');
+		}
+		const isNickAvailable = await isTeamNickValid(form, pb);
+
+		if (!form.valid || !isNickAvailable) return fail(400, { form });
+
+		return { form };
+	}
+};
---
+++ src/routes/team/[slug]/settings/schema.ts
@@ -0,0 +1,160 @@
+import { GenderKind, UserVisiblityKind } from '$tsTypes/user';
+import CodiceFiscale from 'codice-fiscale-js';
+import { Biohazard } from 'lucide-svelte';
+import { z } from 'zod';
+
+const MEGA5 = 5000000;
+const MEGA2 = 2000000;
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+
+export const schema = z.object({
+	name: z.string({
+		required_error: 'Il nome è obbligatorio',
+		invalid_type_error: 'Il nome deve essere una stringa'
+	}),
+	lastName: z.string({
+		required_error: 'Il cognome è obbligatorio',
+		invalid_type_error: 'Il cognome deve essere una stringa'
+	}),
+	birthDate: z
+		.date({
+			required_error: 'La data di nascita è obbligatoria',
+			invalid_type_error: 'La data di nascita deve essere una data'
+		})
+		.min(new Date('1900-01-01'), {
+			message: 'La data di nascita deve essere successiva al 01/01/1900'
+		})
+		.max(new Date(), {
+			message: 'La data di nascita non può essere futura'
+		}),
+	gender: z.nativeEnum(GenderKind, {
+		errorMap: (gender, _ctx) => {
+			switch (gender.code) {
+				case 'invalid_type':
+					return { message: 'Il sesso è richiesto, scegliere tra uno dei seguenti' };
+				case 'invalid_enum_value':
+					return { message: 'Il sesso è richiesto, scegliere tra uno dei seguenti' };
+				default:
+					return { message: 'Il sesso è richiesto, scegliere tra uno dei seguenti' };
+			}
+		}
+	}),
+	prefix: z.string({
+		required_error: 'Il prefisso è obbligatorio',
+		invalid_type_error: 'Il prefisso deve essere una stringa'
+	}),
+	phone: z.string({
+		required_error: 'Il numero di telefono è obbligatorio',
+		invalid_type_error: 'Il numero di telefono deve essere una stringa'
+	}),
+	fiscalCode: z.optional(z.string()).refine(
+		(val) => {
+			if (!val) return true; // Skip validation if value is not provided
+			console.log('checking cf', val, CodiceFiscale.check(val.toUpperCase()));
+			return CodiceFiscale.check(val.toUpperCase());
+		},
+		{
+			message: 'Il codice fiscale inserito non è valido'
+		}
+	),
+	nick: z
+		.string({
+			required_error: 'Il nome utente è obbligatorio',
+			invalid_type_error: 'Il nome utente deve essere una stringa'
+		})
+		.min(3, {
+			message: 'Il nome utente deve avere almeno 3 caratteri'
+		})
+		.max(100, {
+			message: 'Il nome utente deve avere al massimo 100 caratteri'
+		})
+		.regex(/^[a-z0-9\-]+$/, {
+			message: 'Il nome utente può contenere solo minuscole, numeri e trattini'
+		}),
+	visibility: z.nativeEnum(UserVisiblityKind, {
+		errorMap: (gender, _ctx) => {
+			switch (gender.code) {
+				case 'invalid_type':
+					return {
+						message: `La visibilità dell'account è richiesta, scegliere tra uno dei seguenti`
+					};
+				case 'invalid_enum_value':
+					return {
+						message: `La visibilità dell'account è richiesta, scegliere tra uno dei seguenti`
+					};
+				default:
+					return {
+						message: `La visibilità dell'account è richiesta, scegliere tra uno dei seguenti`
+					};
+			}
+		}
+	}),
+	avatarOriginal: z.optional(
+		z
+			.any()
+			.refine((file) => file?.size <= MEGA2, 'Il file deve essere di dimensioni inferiori a 2MB')
+			.refine(
+				(file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+				'Solo formati .jpg, .jpeg, .png e .webp sono supportati'
+			)
+	),
+	avatarCroppedInfo: z.optional(
+		z.object({
+			x: z.number().min(0),
+			y: z.number().min(0),
+			width: z.number().min(0),
+			height: z.number().min(0)
+		})
+	),
+	avatarCropped: z.optional(
+		z
+			.any()
+			.refine(
+				(file) => file?.size <= MEGA2,
+				'Il file croppato deve essere di dimensioni inferiori a 2MB'
+			)
+			.refine(
+				(file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+				'Solo formati .jpg, .jpeg, .png e .webp sono supportati'
+			)
+	),
+	bannerOriginal: z.optional(
+		z
+			.any()
+			.refine((file) => file?.size <= MEGA2, 'Il file deve essere di dimensioni inferiori a 2MB')
+			.refine(
+				(file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+				'Solo formati .jpg, .jpeg, .png e .webp sono supportati'
+			)
+	),
+	bannerCroppedInfo: z.optional(
+		z.object({
+			x: z.number().min(0),
+			y: z.number().min(0),
+			width: z.number().min(0),
+			height: z.number().min(0)
+		})
+	),
+	bannerCropped: z.optional(
+		z
+			.any()
+			.refine(
+				(file) => file?.size <= MEGA2,
+				'Il file croppato deve essere di dimensioni inferiori a 2MB'
+			)
+			.refine(
+				(file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+				'Solo formati .jpg, .jpeg, .png e .webp sono supportati'
+			)
+	),
+	bio: z.optional(
+		z
+			.string({
+				required_error: 'La biografia è obbligatoria',
+				invalid_type_error: 'La biografia deve essere una stringa'
+			})
+			.max(500, {
+				message: 'La biografia deve avere al massimo 500 caratteri'
+			})
+	)
+});

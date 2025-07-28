c31988cbb3213cb50911562499df14901e0cdc8f
vano2903 <davidevanoncini2003@gmail.com>
2025-07-28 18:33:06 +0200

    fix: set start check on regex

--- src/lib/server/hooks/publicPathes.ts
+++ src/lib/server/hooks/publicPathes.ts
@@ -3,24 +3,25 @@ export const isPublicPath = (path: string): boolean => {
 	path = path.toLowerCase();
 
 	const publicPaths = [
-		'\/$',
-		'\/chi-siamo$',
-		'\/login$',
-		'\/register$',
-		'\/forgot-password$',
-		'\/gallery$',
-		'\/championships$',
-		'\/championships\/sheetData$',
-		'\/users$',
-		'\/user\/[a-z0-9_-]+',
-		'\/users\/[a-z0-9_-]+',
-		'\/teams$',
-		'\/team\/[a-z0-9_-]+',
-		'\/teams\/[a-z0-9_-]+'
+		'^\/$',
+		'^\/chi-siamo$',
+		'^\/login$',
+		'^\/register$',
+		'^\/forgot-password$',
+		'^\/gallery$',
+		'^\/championships$',
+		'^\/championships\/sheetData$',
+		'^\/users$',
+		'^\/user\/[a-z0-9_-]+',
+		'^\/users\/[a-z0-9_-]+',
+		'^\/teams$',
+		'^\/team\/[a-z0-9_-]+',
+		'^\/teams\/[a-z0-9_-]+'
 	];
 	const publicPathsRegex = publicPaths
 		// .map((p) => p.replace(/[.*+?^${}()|[\]\\/]/g, '\\$&'))
 		.join('|');
+	console.log('Public paths regex:', publicPathsRegex);
 	const re = new RegExp(publicPathsRegex, 'i');
 	return re.test(path);
 	// if (re.test(path)) {

6d2fbb7ad0f050e546406f57f5b8809e3bcdbd90
MoraGames <andreamorandi2612@gmail.com>
2025-07-31 14:33:44 +0200

    feat: add leaderboards as event result template if event isn't currently onAir

--- src/routes/championships/+page.svelte
+++ src/routes/championships/+page.svelte
@@ -40,7 +40,7 @@
 		'Finale'
 	];
 	let selectedCategory = $state('SoapBox');
-	
+
 	let selectedLeaderboard = $state('Manche 1');
 
 	// variables and functions to manage the content of leaderboard sheet, retrieved by a controlled polling on the Google Sheets API
@@ -156,12 +156,58 @@
 		return elementsList;
 	}
 
-	function testIcons() {
-		const showIcon = foundChampionshipDerived.expand.events.some((v) => {
-			return v.onAir === true;
-		});
-		console.log('testIcons', showIcon);
-	}
+	let eventResults = [
+		{
+			name: 'Giuria Creativa',
+			path: '2025/02-Bergamo/creatività.pdf',
+			date: '26/03/2025 18:53'
+		},
+		{
+			name: 'Giuria Tecnica',
+			path: '2025/02-Bergamo/tecnica.pdf',
+			date: '26/03/2025 18:54'
+		},
+		{
+			name: 'Voti del Pubblico',
+			path: '2025/02-Bergamo/pubblico.pdf',
+			date: '27/03/2025 18:00'
+		},
+		{
+			name: 'Originalità',
+			path: '2025/02-Bergamo/originalità.pdf',
+			date: '26/03/2025 18:01'
+		},
+		{
+			name: 'Stage 1',
+			path: '2025/02-Bergamo/stage-1.pdf',
+			date: '27/03/2025 15:20'
+		},
+		{
+			name: 'Stage 2',
+			path: '2025/02-Bergamo/stage-2.pdf',
+			date: '27/03/2025 17:43'
+		},
+		{
+			name: 'Velocità',
+			path: '2025/02-Bergamo/velocità.pdf',
+			date: '27/03/2025 17:44'
+		},
+		{
+			name: 'Special Stage',
+			path: '2025/02-Bergamo/stage-special.pdf',
+			date: '27/03/2025 18:37'
+		},
+		{
+			name: 'Finale',
+			path: '2025/02-Bergamo/finale.pdf',
+			date: '27/03/2025 18:42'
+		}
+	];
+	let championshipResult = {
+		name: 'Campionato 2025',
+		path: '2025/campionato.pdf',
+		date: '27/03/2025 19:00'
+	};
 </script>
 
 <main class="px-5 pb-16 lg:px-15">
@@ -180,7 +226,7 @@
 			/>
 		</div>
 
-		<div class="my-5 flex flex-row justify-center gap-4">
+		<div class="flex flex-row justify-center gap-4">
 			{#each foundChampionshipDerived.expand.events as event}
 				<button
 					class="hover:underline {foundEventDerived?.id === event.id ? 'text-red-600' : ''}"
@@ -191,14 +237,14 @@
 			{/each}
 		</div>
 
-		<div class="my-5 flex flex-col items-center justify-center gap-4">
+		<div class="my-4 flex flex-col items-center justify-center gap-4">
 			{#each warningsDerived as warning}
 				<span class="rounded-md bg-amber-100 p-5">{warning}</span>
 			{/each}
 		</div>
 	</div>
 
-	<div class="space-y-16">
+	<div class="space-y-8">
 		{#if foundEventDerived?.onAir}
 			<section class="flex flex-col items-center gap-2">
 				<h1 class="text-3xl font-bold">Classifica Live:</h1>
@@ -208,8 +254,8 @@
 						onchange={updateSheet}
 						class="rounded-xl bg-neutral-100 px-1.5 py-0.5"
 					>
-						{#each categories as cat}
-							<option value={cat}>{cat}</option>
+						{#each categories as category}
+							<option value={category}>{category}</option>
 						{/each}
 					</select>
 					<select
@@ -217,31 +263,84 @@
 						onchange={updateSheet}
 						class="rounded-xl bg-neutral-100 px-1.5 py-0.5"
 					>
-						{#each leaderboards as board}
-							<option value={board}>{board}</option>
+						{#each leaderboards as leaderboard}
+							<option value={leaderboard}>{leaderboard}</option>
 						{/each}
 					</select>
 				</div>
-				<div class="mt-4 w-95/100 max-w-full overflow-x-auto md:w-8/10">
+				<div class="mt-4 w-full overflow-x-auto md:w-8/10">
 					{@html sheetHTML}
 				</div>
 			</section>
 		{/if}
-		{#if null != null}
-			<section class="mx-auto p-4">
-				<h1 class="mb-4 text-2xl font-bold">Live Leaderboard</h1>
-				<div class="w-95/100 max-w-full overflow-x-auto md:w-8/10">
-					{@html sheetHTML}
-				</div>
-			</section>
-		{/if}
-		{#if null != null}
-			<section class="mx-auto p-4">
-				<h1 class="mb-4 text-2xl font-bold">Live Leaderboard</h1>
-				<div class="w-95/100 max-w-full overflow-x-auto md:w-8/10">
-					{@html sheetHTML}
+		{#if !foundEventDerived?.onAir && eventResults}
+			<section class="flex flex-col items-center gap-2">
+				<h1 class="text-3xl font-bold">Classifiche Evento:</h1>
+				<div class="flex w-full flex-col gap-2 md:w-8/10">
+					{#each eventResults as result}
+						<div
+							class="xs:m-auto xs:w-8/10 flex flex-col justify-center rounded-md bg-neutral-50 p-2 shadow-sm md:w-6/10"
+						>
+							<div class="flex flex-row content-start justify-between">
+								<span class="font-semibold">{result.name}:</span>
+								<span class="line-clamp-2 justify-self-end text-right text-xs text-neutral-400"
+									>{result.date}</span
+								>
+							</div>
+							<a
+								class="mt-1 ml-3 text-sm text-gray-600 hover:text-red-600"
+								target="_blank"
+								href={'results/' + result.path}
+							>
+								<div class="flex items-center gap-1">
+									<img
+										src="https://www.boxrally.eu/boxrally/images/pdf_icon.gif"
+										alt="PDF Icon"
+										class="mr-1 shrink-0"
+									/>
+									<span class="xs:text-sm line-clamp-2 text-xs underline">{result.path}</span>
+								</div>
+							</a>
+						</div>
+					{/each}
+					{#if championshipResult}
+						<hr class="my-2" />
+						<div
+							class="xs:m-auto xs:w-8/10 flex flex-col justify-center rounded-md bg-neutral-200 p-2 shadow-sm md:w-6/10"
+						>
+							<div class="flex flex-row content-start justify-between">
+								<span class="font-semibold">{championshipResult.name}:</span>
+								<span class="justify-self-end text-right text-xs text-neutral-500"
+									>{championshipResult.date}</span
+								>
+							</div>
+							<a
+								class="mt-1 ml-3 text-sm text-gray-600 hover:text-red-600"
+								target="_blank"
+								href={'results/' + championshipResult.path}
+							>
+								<div class="flex items-center gap-1">
+									<img
+										src="https://www.boxrally.eu/boxrally/images/pdf_icon.gif"
+										alt="PDF Icon"
+										class="mr-1 shrink-0"
+									/>
+									<span class="xs:text-sm line-clamp-2 text-xs underline"
+										>{championshipResult.path}</span
+									>
+								</div>
+							</a>
+						</div>
+					{/if}
 				</div>
 			</section>
 		{/if}
 	</div>
 </main>
+
+<style>
+	button,
+	a {
+		cursor: pointer;
+	}
+</style>

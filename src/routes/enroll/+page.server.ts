7c76b4555af7dcc3ccffb3bcdc0334256187451f
vano2903 <davidevanoncini2003@gmail.com>
2025-08-26 20:21:03 +0200

    feat: enroll server handling, WIP ui

--- src/routes/enroll/+page.server.ts
+++ src/routes/enroll/+page.server.ts
@@ -1,8 +1,28 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
+import type { TypedPocketBase } from '$types/pocketbase/pocketbase';
+import type { EventNonExpand } from '$types/pocketbase/event';
+
+async function findLatestEventWithOpenSubscriptions(pb: TypedPocketBase) {
+	const [event, eventErr] = (await goCatch(
+		pb.collection('events').getFirstListItem(`subscriptionsOpen=true`, {
+			sort: '-startDate'
+		})
+	)) as [EventNonExpand | undefined, undefined | Error];
+	return [event, eventErr] as [EventNonExpand | undefined, undefined | Error];
+}
+
+async function findEventByShortName(pb: TypedPocketBase, shortName: string) {
+	const [event, eventErr] = (await goCatch(
+		pb.collection('events').getFirstListItem(`shortName="${shortName}"`)
+	)) as [EventNonExpand | undefined, undefined | Error];
+	return [event, eventErr] as [EventNonExpand | undefined, undefined | Error];
+}
+
+export const load: PageServerLoad = async ({ locals, url }) => {
 	const { user, pb } = locals;
+	const warnings: string[] = [];
 
 	if (!user) {
 		throw redirect(303, '/login');
@@ -10,16 +30,96 @@ export const load: PageServerLoad = async ({ locals }) => {
 
 	if (!locals.teamsCount) {
 		console.log("getting user's teams", user.person);
-		const [res, err] = await goCatch(
+		const [res, teamErr] = await goCatch(
 			pb.collection('teams').getList(1, 1, {
 				filter: `members:each ?= "${user.person}" || owner.id = "${user.person}"`
 			})
 		);
-		console.log('teams in layout server dash/:', res);
+		if (teamErr) {
+			console.error("error getting user's teams:", teamErr);
+			locals.teamsCount = 0;
+			// throw error(500, 'Errore nel recupero delle squadre dell\'utente.');
+		}
 		locals.teamsCount = res?.totalItems ?? 0;
 	}
 
+	// const [championshipsList, lastOngoingChampionshipIndex, championshipsError] =
+	// 	await getChampionshipsList(pb);
+
+	// if (championshipsError || !championshipsList || championshipsList.length === 0) {
+	// 	console.error('Error fetching championships: ', championshipsError);
+	// 	throw fail(500);
+	// }
+
+	// const selectedYear = url.searchParams.get('year');
+	const selectedEvent = url.searchParams.get('event');
+	console.log('Selected event:', selectedEvent);
+
+	const [latestEvent, latestEventErr] = await findLatestEventWithOpenSubscriptions(pb);
+	if (latestEventErr || !latestEvent) {
+		warnings.push('Non sono presenti eventi con le iscrizioni aperte.');
+		return {
+			// championshipsList,
+			// lastOngoingChampionshipIndex,
+			event: {
+				name: 'nessun evento disponibile'
+			} as EventNonExpand,
+			teamsCount: locals.teamsCount,
+			warnings
+		};
+	}
+
+	if (!selectedEvent) {
+		throw redirect(303, `/enroll?event=${latestEvent.shortName}`);
+	}
+
+	let event: EventNonExpand = latestEvent;
+	if (selectedEvent) {
+		if (selectedEvent !== latestEvent.shortName) {
+			// eslint-disable-next-line prefer-const
+			let [foundEvent, foundEventErr] = await findEventByShortName(pb, selectedEvent);
+			if (foundEventErr || !foundEvent) {
+				warnings.push(`L'evento "${selectedEvent}" non è stato trovato.`);
+			}
+			event = foundEvent || latestEvent;
+		}
+	}
+
+	if (!event.subscriptionsOpen) {
+		warnings.push(`Le iscrizioni per l'evento "${selectedEvent}" sono chiuse.<br><br>
+		Attualmente l'evento con le iscrizioni aperte più recente è<br><a class="link" href="/enroll?event=${latestEvent.shortName}">${latestEvent.name}</a>.`);
+	} else if (
+		(event.startDate && new Date(event.startDate).valueOf() < new Date().valueOf()) ||
+		event.numSubscriptions >= (event.maxSubscriptions || 0)
+	) {
+		warnings.push(`Le iscrizioni per l'evento "${selectedEvent}" sono chiuse.`);
+	}
+
+	// const [event, eventErr] = (await goCatch(
+	// 	pb.collection('events').getFirstListItem(`shortName="${selectedEvent}"`)
+	// )) as [EventNonExpand | undefined, undefined | Error];
+	// if (eventErr || !event) {
+	// 	warnings.push(`L'evento "${selectedEvent}" non è stato trovato.`);
+	// }
+
+	// const [championshipsList, err] = (await goCatch(
+	// 	pb.collection('championships').getFullList({ sort: '+startDate' })
+	// )) as [ChampionshipNonExpand[], undefined] | [undefined, Error];
+	// if (championshipsError || !championshipsList || championshipsList.length === 0) {
+	// 	console.error('Error fetching championships: ', championshipsError);
+	// 	throw fail(500);
+	// }
+	// const lastOngoingChampionshipIndex = championshipsList.findLastIndex((v) => {
+	// 	return v.ongoing;
+	// });
+
+	// return { championshipsList, lastOngoingChampionshipIndex };
+	console.log('event in load', event);
 	return {
-		teamsCount: locals.teamsCount
+		// championshipsList,
+		// lastOngoingChampionshipIndex,
+		event,
+		teamsCount: locals.teamsCount,
+		warnings
 	};
 };
--- src/routes/enroll/+page.svelte
+++ src/routes/enroll/+page.svelte
@@ -1,38 +1,78 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	let { data } = $props();
+	console.log('date in enroll page', data);
+	let warnings = $derived(data.warnings);
+	let event = $derived(data.event);
 
-	let event = $state('');
-
-	onMount(() => {
-		const url = new URL(window.location.href);
-		console.log(url);
-		event = url.searchParams.get('event') || 'Evento non specificato';
-	});
+	function formatDate(date: Date | undefined) {
+		if (!date) {
+			return null;
+		}
+		return new Date(date).toLocaleDateString('it-IT', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+			hour: '2-digit',
+			minute: '2-digit'
+		});
+	}
 </script>
 
 <main>
-	<div class="m-auto mt-10 w-1/2 rounded-md bg-neutral-100 p-4 shadow-md">
-		<div class="flex flex-col gap-4 text-center text-base">
-			<hr
-				class="mx-auto mt-2 mb-1.5 h-0.75 w-2/3 max-w-70 rounded-sm border-0 bg-red-600 md:mt-4"
-			/>
-			<h2 class="text-2xl font-bold">
-				{event}
-				<!-- {url.searchParams.get('event')} -->
-				<!-- {url.searchParams.get('event') || 'Evento non specificato'} -->
-			</h2>
-			<p>
-				La pagina non esiste realmente, volevo solo verificare se la redirect funzionasse
-				correttamente.
-			</p>
-			<div class="mt-4 flex flex-row flex-wrap justify-center gap-6 text-gray-600 underline">
-				<a href="/calendars" class="hover:text-red-600">Torna al Calendario</a>
-				<a href="/championships" class="hover:text-red-600">Torna al Campionato</a>
+	<div class="m-auto mt-10 rounded-md bg-neutral-100 p-2 shadow-md md:w-2/3 md:p-4 lg:w-1/2">
+		<div class="flex flex-col gap-4 overflow-hidden text-center text-base">
+			<div class="relative h-1/3">
+				<img
+					src="/images/calendars/eventCover.png"
+					alt={event.name}
+					class="h-full w-full object-cover"
+				/>
 			</div>
-			<hr
-				class="mx-auto mt-2 mb-1.5 h-0.75 w-2/3 max-w-70 rounded-sm border-0 bg-red-600 md:mt-4"
-			/>
+			<div class="flex flex-col content-between items-center p-1 text-center">
+				<hr
+					class="mx-auto mt-1.5 mb-2 h-0.75 w-[80%] max-w-[20rem] rounded-sm border-0 bg-red-600 md:mb-4 md:w-2/3 md:max-w-70"
+				/>
+				<div class="space-y-2">
+					<p class="hidden text-xl text-gray-600 md:block">— {event.shortName} —</p>
+					<h3 class="text-2xl font-bold md:px-5 md:text-3xl">{event.name}</h3>
+					<div class="mt-2 text-lg text-gray-500 md:mt-4 md:text-base">
+						<div class="block md:hidden">
+							<p>{formatDate(event.startDate)}</p>
+						</div>
+						<div class="hidden md:block">
+							<p>dal: {formatDate(event.startDate)}</p>
+							{#if event.endDate}
+								<p>al: {formatDate(event.endDate)}</p>
+							{:else}
+								<p>&nbsp</p>
+							{/if}
+						</div>
+					</div>
+					<p class="text-md mt-2 text-gray-500 md:text-base">
+						{event.numSubscriptions}{(event.maxSubscriptions ?? 0) > 0
+							? `/${event.maxSubscriptions}`
+							: ''} iscrizioni
+					</p>
+				</div>
+				<hr
+					class="mx-auto mt-2 mb-1.5 h-0.75 w-2/3 max-w-70 rounded-sm border-0 bg-red-600 md:mt-4"
+				/>
+			</div>
 		</div>
+
+		<div class="mb-4 flex flex-col items-center justify-center gap-4">
+			{#each warnings as warning}
+				<span class="rounded-md bg-amber-100 p-5">
+					{@html warning}
+				</span>
+			{/each}
+		</div>
+
+		<div class="mb-4 flex flex-row flex-wrap justify-center gap-6 text-gray-600 underline">
+			<a href="/calendars" class="hover:text-red-600">Torna al Calendario</a>
+			<a href="/championships" class="hover:text-red-600">Torna al Campionato</a>
+		</div>
+		<hr class="mx-auto mt-2 mb-1.5 h-0.75 w-2/3 max-w-70 rounded-sm border-0 bg-red-600 md:mt-4" />
 	</div>
 </main>
 

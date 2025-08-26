44d058a55a5363d57e6a5b2e0139ff99fdb741a1
vano2903 <davidevanoncini2003@gmail.com>
2025-08-26 20:20:43 +0200

    feat: better error messages when event is not found

--- src/routes/calendars/+page.ts
+++ src/routes/calendars/+page.ts
@@ -1,13 +1,13 @@
-import type { PageLoad } from "./$types";
+import type { PageLoad } from './$types';
 
 import pocketbase from 'pocketbase';
 import { env } from '$env/dynamic/public';
 import type { TypedPocketBase } from '$types/pocketbase/pocketbase';
 import type { ChampionshipExpand } from '$types/pocketbase/championship';
-import { fail } from "@sveltejs/kit";
+import { fail } from '@sveltejs/kit';
 // import type { Result } from "$types/pocketbase/results";
 
-export const load: PageLoad = async ({ data, url }) => {
+export const load: PageLoad = async ({ data, url, fetch }) => {
 	console.log('Loading calendars:\n > data = ', data, '\n > url = ', url);
 
 	// destructures the data received from the PageServerLoad and prepare the variables
@@ -16,21 +16,37 @@ export const load: PageLoad = async ({ data, url }) => {
 
 	// retrieve the selected championship or, if nullish, the last ongoingChampionship available
 	const pb = new pocketbase(env.PUBLIC_PB_INSTANCE) as TypedPocketBase;
-	const requestedChampionshipIndex = championshipsList.findIndex((c: { name: string | null; }) => { return (c.name === url.searchParams.get('championship')) });
+	const requestedChampionshipIndex = championshipsList.findIndex((c: { name: string | null }) => {
+		return c.name === url.searchParams.get('championship');
+	});
 
 	if (requestedChampionshipIndex === -1 && url.searchParams.get('championship')) {
-		warnings.push(`Il campionato "${url.searchParams.get('championship')}" non è stato trovato.`);
+		const searchedChampionship = Number(url.searchParams.get('championship'));
+		if (Number(championshipsList.at(lastOngoingChampionshipIndex)?.name) < searchedChampionship) {
+			warnings.push(
+				`Il campionato "${url.searchParams.get('championship')}" non è ancora iniziato.`
+			);
+		} else {
+			warnings.push(`Il campionato "${url.searchParams.get('championship')}" non è stato trovato.`);
+		}
 	}
-	const researchChampionshipIndex = (requestedChampionshipIndex !== -1 ? requestedChampionshipIndex : lastOngoingChampionshipIndex);
+
+	const researchChampionshipIndex =
+		requestedChampionshipIndex !== -1 ? requestedChampionshipIndex : lastOngoingChampionshipIndex;
 
 	// retrieve the ChampionshipExpand version (with full events list)
 	let foundChampionship: ChampionshipExpand;
 	try {
-		foundChampionship = await pb.collection('championships').getFirstListItem(`id="${championshipsList.at(researchChampionshipIndex)?.id}"`, { expand: 'events,events.location' });
+		foundChampionship = await pb
+			.collection('championships')
+			.getFirstListItem(`id="${championshipsList.at(researchChampionshipIndex)?.id}"`, {
+				fetch: fetch,
+				expand: 'events,events.location'
+			});
 	} catch (err) {
 		console.error('Championship not found: ', err);
-		throw fail(500)
+		throw fail(500);
 	}
 
-	return { championshipsList, foundChampionship, warnings }
+	return { championshipsList, foundChampionship, warnings };
 };
--- src/routes/championships/+page.ts
+++ src/routes/championships/+page.ts
@@ -9,20 +9,28 @@ import type { Result } from '$types/pocketbase/results';
 
 export const load: PageLoad = async ({ data, url, fetch }) => {
 	console.log('Loading championships:\n > data = ', data, '\n > url = ', url);
+	const warnings: string[] = [];
+	const pb = new pocketbase(env.PUBLIC_PB_INSTANCE) as TypedPocketBase;
 
 	// destructures the data received from the PageServerLoad and prepare the variables
 	const { championshipsList, lastOngoingChampionshipIndex } = data;
-	const warnings: string[] = [];
 
 	// retrieve the selected championship or, if nullish, the last ongoingChampionship available
-	const pb = new pocketbase(env.PUBLIC_PB_INSTANCE) as TypedPocketBase;
 	const requestedChampionshipIndex = championshipsList.findIndex((c: { name: string | null }) => {
 		return c.name === url.searchParams.get('championship');
 	});
 
 	if (requestedChampionshipIndex === -1 && url.searchParams.get('championship')) {
-		warnings.push(`Il campionato "${url.searchParams.get('championship')}" non è stato trovato.`);
+		const searchedChampionship = Number(url.searchParams.get('championship'));
+		if (Number(championshipsList.at(lastOngoingChampionshipIndex)?.name) < searchedChampionship) {
+			warnings.push(
+				`Il campionato "${url.searchParams.get('championship')}" non è ancora iniziato.`
+			);
+		} else {
+			warnings.push(`Il campionato "${url.searchParams.get('championship')}" non è stato trovato.`);
+		}
 	}
+
 	const researchChampionshipIndex =
 		requestedChampionshipIndex !== -1 ? requestedChampionshipIndex : lastOngoingChampionshipIndex;
 
@@ -32,8 +40,8 @@ export const load: PageLoad = async ({ data, url, fetch }) => {
 		foundChampionship = await pb
 			.collection('championships')
 			.getFirstListItem(`id="${championshipsList.at(researchChampionshipIndex)?.id}"`, {
-				expand: 'events',
-				fetch: fetch
+				expand: 'events'
+				// fetch: fetch
 			});
 	} catch (err) {
 		console.error('Championship not found: ', err);
@@ -47,7 +55,7 @@ export const load: PageLoad = async ({ data, url, fetch }) => {
 	if (requestedEventIndex === -1) {
 		if (url.searchParams.get('event')) {
 			warnings.push(
-				`L'evento "${url.searchParams.get('event')}" non esiste per il campionato "${url.searchParams.get('championship')}".`
+				`L'evento "${url.searchParams.get('event')}" non esiste per il campionato "${url.searchParams.get('championship') || championshipsList.at(researchChampionshipIndex)?.name}".`
 			);
 		}
 		requestedEventIndex = foundChampionship.expand.events.findIndex((e) => {
@@ -62,7 +70,10 @@ export const load: PageLoad = async ({ data, url, fetch }) => {
 		try {
 			const expandedEvent = await pb
 				.collection('events')
-				.getFirstListItem(`id="${foundEvent.id}"`, { expand: 'results', fetch: fetch });
+				.getFirstListItem(`id="${foundEvent.id}"`, {
+					expand: 'results'
+					// , fetch: fetch
+				});
 			responseEventResults = expandedEvent.expand?.results;
 		} catch (err) {
 			console.error('Event not found: ', err);

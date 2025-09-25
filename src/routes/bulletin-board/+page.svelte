8a684e8cf8b33513563974e94c00781194537423
MoraGames <andreamorandi2612@gmail.com>
2025-09-26 00:35:01 +0200

    feat: add the bulletin-board page template

---
+++ src/routes/bulletin-board/+page.svelte
@@ -0,0 +1,14 @@
+<main class="pb-16">
+	<header class="flex flex-col items-center space-y-2 px-8 py-15 text-center">
+		<span class="text-5xl font-bold"> Bacheca </span>
+		<p class="text-neutral-500">
+			La nostra board è come il vecchio tabellone di paese: ogni foglio racconta un pezzo della
+			gara. Avvisi, regole e info utili, ma sempre con la stessa dose di entusiasmo che ci fa
+			correre giù per le strade.
+		</p>
+	</header>
+	<div class="space-y-16"></div>
+</main>
+
+<style>
+</style>

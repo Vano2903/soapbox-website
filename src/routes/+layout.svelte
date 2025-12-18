8423acb426790bfe0899292008b31627c40a57e0
davde vanoncini <davidevanoncini2003@gmail.com>
2025-12-18 14:55:44 +0100

    fix: set correct og and twitter meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,25 +7,33 @@
 	import type { User } from '$types/pocketbase/user';
 	import { env } from '$env/dynamic/public';
 	import type { Snippet } from 'svelte';
+
+	import { page } from '$app/state';
+
 	// import Navbar from '$components/navbar/Navbar.svelte';
 	// import Navbar3 from '$components/navbar/Navbar3.svelte';
 
 	interface Props {
 		children: Snippet;
 		data: { user: User };
-		ogInfo: {
-			title: string;
-			description: string;
+		ogInfo?: {
+			title?: string;
+			description?: string;
 			image?: string;
-			url: string;
+			// url: string;
 			keywords?: string[];
 		};
 	}
 	let { children, data, ogInfo }: Props = $props();
-	ogInfo.keywords = ogInfo.keywords ?? ['box rally', 'bergamo', 'rally', 'soap box'];
-	const baseUrl = env.PUBLIC_BASE_URL ?? 'http://localhost:5173';
-	ogInfo.url = new URL(ogInfo.url, baseUrl).toString();
-	ogInfo.image = ogInfo.image ?? new URL('images/carousel/8.jpeg', baseUrl).toString();
+	const keywords = ogInfo?.keywords ?? ['box rally', 'bergamo', 'rally', 'soap box'];
+	const baseUrl = 'https://' + (env.PUBLIC_BASE_URL ?? 'localhost:5173');
+	const url = new URL(page.url.pathname, baseUrl).toString();
+	const image = ogInfo?.image ?? new URL('images/carousel/8.jpeg', baseUrl).toString();
+	const siteName = 'BoxRally - Sito ufficiale delle SoapBox';
+	const title = ogInfo?.title ?? 'Il Garage del SoapBox Rally Club';
+	const description =
+		ogInfo?.description ??
+		'Scopri tutti gli eventi, news, foto e video del gruppo sportivo SoapBox Rally: il downhill fatto in legno e su misura.';
 </script>
 
 <svelte:head>
@@ -35,21 +43,21 @@
 		href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 		rel="stylesheet"
 	/>
-	<title>{ogInfo.title}</title>
-	<meta name="description" content={ogInfo.description} />
-	<meta property="og:title" content={ogInfo.title} />
-	<meta property="og:description" content={ogInfo.description} />
-	<meta property="og:image" content={ogInfo.image} />
-	<meta property="og:url" content={ogInfo.url} />
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+	<meta property="og:url" content={url} />
+	<meta property="og:site_name" content={siteName} />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="it_IT" />
+	<meta name="twitter:creator" content="@soapboxrally" />
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={ogInfo.title} />
-	<meta name="twitter:description" content={ogInfo.description} />
-	<meta name="twitter:image" content={ogInfo.image} />
-	{#if ogInfo.keywords}
-		<meta name="keywords" content={ogInfo.keywords.join(', ')} />
-	{:else}
-		<meta name="keywords" content="" />
-	{/if}
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content={image} />
+	<meta name="keywords" content={keywords.join(', ')} />
 </svelte:head>
 
 <!-- <ModeWatcher /> -->
